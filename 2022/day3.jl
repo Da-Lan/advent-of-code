af2c7a61df6f78e5db402b298cedc30ddb5646ea
Da-Lan <dl.nguyen404@gmail.com>
2022-12-06 23:56:40 +0100

    add: Added solution script for 2022 day 3 problem 1

---
+++ 2022/day3.jl
@@ -0,0 +1,42 @@
+using Downloads: download
+using DelimitedFiles
+using DataFrames
+using CSV
+
+
+##############################
+# Load data
+##############################
+
+session_cookie = read(string(@__DIR__, "\\", "session_cookie.txt"), String)
+
+# Credits to https://www.incidentalfindings.org/posts/2022-01-24_learning-julia-with-adventofcode/
+# for this download code !
+url = "https://adventofcode.com/2022/day/3/input"
+filename = "day3.csv"
+
+data_file = joinpath(@__DIR__, filename)
+println("Day 3: $data_file")
+download(url, data_file, headers = Dict("cookie" => "session=$(session_cookie)"))
+df = DataFrame(CSV.File(data_file, header=false, ignoreemptyrows=false))
+
+
+##############################
+# Extract solution 1
+##############################
+
+priorities = map(x -> (x1 = x[1];
+                        if islowercase(x1)
+                            parse(Int, x1, base=62) - 35
+                        elseif isuppercase(x1)
+                            parse(Int, x1, base=62) - 9 + 26
+                        end),
+                    map(x -> intersect(x[1], x[2]),
+                        map(x -> [x[1:Int(floor(length(x)/2))],
+                                x[Int(floor(length(x)/2))+1:length(x)]],
+                        df[!, "Column1"])
+                    )
+                )
+
+                
+println("Solution: ", sum(priorities))
